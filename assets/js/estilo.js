265811ee9e26b0b1777c7683f0e0b08e52c26689
Gustavo Matos <gustavofisica@gmail.com>
2021-06-29 15:28:01 -0300

    Importando conteúdo do portfólio
    Inserindo HTML do portfólio de imagens no index, reconfigurando função JS do Slider e iniciando estilização CSS.

--- assets/js/estilo.js
+++ assets/js/estilo.js
@@ -49,3 +49,39 @@ for (let i = 0; i < titulo.length; i++) {
     })
     
 }
+
+//Função do Slider
+
+var portifolio = document.querySelector('.conteudo__portifolio');
+if (portifolio != null) {
+    var slideIndex = 1;
+    showSlides(slideIndex);
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("conteudo__portifolio__slideshow__slide");
+        var dots = document.getElementsByClassName("dot");
+        if (n > slides.length) {
+            slideIndex = 1
+        }
+        if (n < 1) {
+            slideIndex = slides.length
+        }
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+    }
+}
