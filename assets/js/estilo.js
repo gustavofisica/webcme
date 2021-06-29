6733a3de428c6fe239d8597f87977ab784e58ff3
Gustavo Matos <gustavofisica@gmail.com>
2021-06-29 18:30:01 -0300

    Estilizando o Slider com CSS Grid
    Reformatando o Slider do projeto anterior com o padrão Grid no CSS.

--- assets/js/estilo.js
+++ assets/js/estilo.js
@@ -81,7 +81,7 @@ if (portifolio != null) {
         for (i = 0; i < dots.length; i++) {
             dots[i].className = dots[i].className.replace(" active", "");
         }
-        slides[slideIndex - 1].style.display = "block";
+        slides[slideIndex - 1].style.display = "grid";
         dots[slideIndex - 1].className += " active";
     }
 }
