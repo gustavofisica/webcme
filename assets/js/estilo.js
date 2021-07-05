3e46735e21a6955697baa9a570c8c786c677b6c9
Gustavo Matos <gustavofisica@gmail.com>
2021-07-05 15:37:39 -0300

    Reformatando imagens para performance
    Reduzindo tamanho de arquivos de imagens para melhorar a performance da página.

--- assets/js/estilo.js
+++ assets/js/estilo.js
@@ -2,14 +2,13 @@
 var backgroundImage = document.querySelector('.cabecalho');
 
 var images = [
-    'assets/img/header/MEV_Espiculas_Bio.png',
-    'assets/img/header/MET_NanoTubo_Fis.png',
-    'assets/img/header/MET_Polimero_Qui.png',
-    'assets/img/header/MEV_Inseto_Bio.png',
-    'assets/img/header/MEV_Inseto_Pan_Bio.png',
-    'assets/img/header/MEV_Planta_Bio.png',
-    'assets/img/header/MEV_Planta_Pan_Bio.png',
-    'assets/img/header/MEV_Planta_Transv_Bio.png'
+    'assets/img/header/MEV_Espiculas_Bio.jpg',
+    'assets/img/header/MET_NanoTubo_Fis.jpg',
+    'assets/img/header/MEV_Inseto_Bio.jpg',
+    'assets/img/header/MEV_Inseto_Pan_Bio.jpg',
+    'assets/img/header/MEV_Planta_Bio.jpg',
+    'assets/img/header/MEV_Planta_Pan_Bio.jpg',
+    'assets/img/header/MEV_Planta_Transv_Bio.jpg'
 ];
 
 var bg = images[Math.floor(Math.random() * images.length)];
